ef80b3d2067a370804a801a460fc58db8776ac29
jin4363 <zxcwlsgh12@naver.com>
2023-08-06 23:22:03 +0900

    aws mfa auth

--- requirement.sh
+++ requirement.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+#Settings before execution
+# aws configure > your aws account information
+# edit : ~/.aws/credential
+# add : aws_arn_mfa : your aws account mfa device
+
 #aws linux
 sudo yum update -y
 sudo yum install jq -y
